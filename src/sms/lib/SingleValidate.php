8b52db6e9a721f19c67134846210fed1afc56c2b
yufeiqiang <1446731380@qq.com>
2020-11-10 12:03:35 +0800

    修改验证规则

--- src/sms/lib/SingleValidate.php
+++ src/sms/lib/SingleValidate.php
@@ -6,18 +6,18 @@ use panthsoni\alibaba\common\CommonValidate;
 class SingleValidate extends CommonValidate
 {
     protected $rule = [
-        'domain|短信发送域名' => 'length:0,50',
-        'method|请求方式' => 'length:0,10',
-        'security|是否安全' => 'in:false,true',
-        'RegionId|地区id' => 'length:0,20',
-        'Action|请求动作' => 'length:0,10',
-        'Version|短信版本' => 'length:0,10',
-        'AccessKeyId|短信key' => 'length:0,50',
-        'AccessKeySecret|短信密钥' => 'length:0,100',
-        'SignName|短信签名' => 'length:0,10',
-        'PhoneNumbers|电话号码' => 'array',
-        'TemplateCode|短信模板code' => 'length:0,15',
-        'TemplateParam|短信模板参数' => 'length:0,50'
+        'domain|短信发送域名domain' => 'length:0,50',
+        'method|请求方式method' => 'length:0,10',
+        'security|是否安全security' => 'in:false,true',
+        'RegionId|地区RegionId' => 'length:0,20',
+        'Action|请求动作Action' => 'length:0,10',
+        'Version|短信版本Version' => 'length:0,10',
+        'AccessKeyId|短信AccessKeyId' => 'length:0,50',
+        'AccessKeySecret|短信密钥AccessKeySecret' => 'length:0,100',
+        'SignName|短信签名SignName' => 'length:0,10',
+        'PhoneNumbers|电话号码PhoneNumbers' => 'array',
+        'TemplateCode|短信模板TemplateCode' => 'length:0,15',
+        'TemplateParam|短信模板参数TemplateParam' => 'length:0,50'
     ];
 
     public $scene = [
